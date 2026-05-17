43e7194c12a4727f79e7303c10da094f30162e65
jaizzrohit6 <rohit1952425@gmail.com>
2026-05-17 11:03:31 +0530

    leetcode restarted 17-05

---
+++ leetcode_started/easy/addPlusOne.js
@@ -0,0 +1,15 @@
+
+function addPlusOne(digits){
+    let n = digits.length-1
+    for(let i = n ; i >= 0;i--){
+        if(digits[i]< 9){
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0
+    }
+    return [1,...digits]
+}
+
+let digits = [1,2,4]
+console.log(addPlusOne(digits))
---
+++ leetcode_started/easy/insertPosition.js
@@ -0,0 +1,20 @@
+
+function insertSearchOption(nums,target){
+    let left = 0
+    let right = nums.length -1
+    while(left <=right){
+        let mid = left + Math.floor((right-left)/2)
+        if(nums[mid] === target){
+            return mid
+        }else if(nums[mid] < target){
+            left = mid+1 
+        }else{
+            right = mid-1
+        }
+    }
+    return left
+}
+
+let nums = [1,3,5,7]
+let target = 8
+console.log(insertSearchOption(nums,target))
