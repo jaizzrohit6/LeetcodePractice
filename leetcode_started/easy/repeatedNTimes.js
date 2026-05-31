645902e214303675cd2cbfde3d475b1a7d1a4cf1
jaizzrohit6 <rohit1952425@gmail.com>
2026-05-31 10:31:02 +0530

    leetcode restarted 31-05

---
+++ leetcode_started/easy/repeatedNTimes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    let obj = {}
+    for(let i = 0;i<nums.length;i++){
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1
+    }
+    for(const keys in obj){
+        if(obj[keys] > 1){
+            return parseInt(keys)
+        }
+    }
+};
